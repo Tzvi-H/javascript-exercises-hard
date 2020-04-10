e74ad32687eef28367bde49b16be8e014317cffe
Tzvi-H <16guitar@gmail.com>
2020-04-10 10:52:55 -0500

    solution 1 diamonds

---
+++ interpretive_problem_solving/diamonds.js
@@ -0,0 +1,57 @@
+/*
+Problem
+  input  => Number (odd integer)
+  output => print a string
+
+  - given an odd number representing the height and width of a grid
+  - print a diamond that fits into the grid
+  - 
+Examples
+  input  => 3
+  output => ' *'  // 1 spaces, 1 stars
+            '***' // 0 spaces, 3 stars
+            ' *'  // 1 spaces, 1 stars
+  input  => 5          
+  output => '  *'   // 2 spaces, 1 star 
+            ' ***'  // 1 spaces, 3 stars
+            '*****' // 0 spaces, 5 stars
+            ' ***'  // 1 spaces, 3 stars
+            '  *'   // 2 spaces, 1 star 
+Data Structure
+  each line is a string
+Algorithm
+  - given a number representing the stars size
+  - stars = 1
+  - for line = 1 <= size
+    - spaces = (size - stars) / 2
+    - print (space * spaces) + (star * stars)
+    - if (before middle line)
+        stars += 2
+        spaces -= 1
+      else (reached the middle line)
+        stars -= 1
+        spaces += 1   
+    
+Questions  
+  - one string or seperate strings?
+*/
+
+function diamond(size) {
+  const STAR = '*'
+  const SPACE = ' ';
+  let starCount = 1;
+  let spaceCount;
+
+  for (line = 1; line <= size; line += 1) {
+    spaceCount = (size - starCount) / 2
+    console.log(SPACE.repeat(spaceCount) + STAR.repeat(starCount));
+
+    if (line < Math.ceil(size / 2)) {
+      starCount += 2;
+    } else {
+      starCount -= 2;
+    }
+  }
+}
+
+diamond(9);

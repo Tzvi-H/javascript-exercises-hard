91ec6c98a6b07a75453296ceb60d9cfea38f49c1
Tzvi-H <16guitar@gmail.com>
2020-04-12 16:56:19 -0500

    complete

---
+++ medium_2/triangle_sides.js
@@ -0,0 +1,36 @@
+function allEqual(side1, side2, side3) {
+  return side1 === side2 && side2 === side3;
+}
+
+function twoEqual(side1, side2, side3) {
+  return side1 === side2 || 
+         side2 === side3 ||
+         side2 === side3;
+}
+
+function validTriangle(side1, side2, side3) {
+  let max = Math.max(side1, side2, side3);
+  let sum = side1 + side2 + side3;
+  return (side1 > 0 && side2 > 0 && side3 > 0) &&
+         (sum - max) > max;
+}
+
+function triangle(side1, side2, side3) {
+  if (!validTriangle(side1, side2, side3)) {
+    return 'invalid';
+  } 
+
+  if (allEqual(side1, side2, side3)) {
+    return 'equilateral';
+  } else if (twoEqual(side1, side2, side3)) {
+    return 'isosceles';
+  } else {
+    return 'scalene';
+  }
+}
+
+console.log(triangle(3, 3, 3));        // "equilateral"
+console.log(triangle(3, 3, 1.5));      // "isosceles"
+console.log(triangle(3, 4, 5));        // "scalene"
+console.log(triangle(0, 3, 3));        // "invalid"
+console.log(triangle(3, 1, 1));        // "invalid"
